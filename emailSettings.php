1bf2eb2d349891caf960abd4680e047edbcbd545
ranilwallace <ranilwallace@gmail.com>
2019-05-09 01:10:42 -0500

    Added contact section

---
+++ emailSettings.php
@@ -0,0 +1,8 @@
+<?php
+$email_to = "ranilwallacebusiness@gmail.com"; // your email address
+$email_subject = "Contact Form Message"; // email subject line
+$thankyou = "index.html"; // thank you page
+
+// Do not change this line
+$base = "CgpGb3JtIHBvd2VyZWQgYnkgaHR0cHM6Ly93d3cuZnJlZWNvbnRhY3Rmb3JtLmNvbQ==";
+?>
